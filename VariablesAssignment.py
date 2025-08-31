34b975d72ea30423558598aa7d56236c9f850d0c
mkara <mkarausch@gmail.com>
2025-08-31 10:56:58 -0400

    Initial commit

---
+++ VariablesAssignment.py
@@ -0,0 +1,13 @@
+answer = 5 * 6
+print("The answer to your query is:",answer)
+answer = round(8 / 3,8)
+print("The answer to your query is:",answer)
+answer = 8 // 3
+print("The answer to your query is:",answer)
+answer = 8 % 3
+print("The answer to your query is:",answer)
+answer = 4200000 + 23090
+print("The answer to your query is:",answer)
+answer = 4200000 - 7633
+print("The answer to your query is:",answer, "\n")
+print("Matthias Karausch")
